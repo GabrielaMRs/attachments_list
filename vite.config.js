b4310a2e53ff390bc164f82e39e5766aeeb605c6
Gabriela <gabrielamrs19@gmail.com>
2024-06-03 17:04:54 -0300

    js adjusted

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/advice-generator-app-main/',
+  build: {
+    outDir: 'dist'
+  }
 })
+
