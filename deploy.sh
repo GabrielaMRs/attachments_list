70847d5dae07d566d84c01b0267036a83f5eb1ff
Gabriela <gabrielamrs19@gmail.com>
2024-06-03 16:59:47 -0300

    deploy adjusted

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
+#!/usr/bin/env sh
+
+set -e
+
+npm run build
+
 cd dist 
 
 git init 
 git add -A
 git commit -m 'New Deployment'
-git push -f git@github.com: GabrielaMRs/attachments_list master:gh-pages
+git push -f git@github.com: GabrielaMRs/attachments_list.git master:gh-pages
+
+cd - 
